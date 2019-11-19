ed8ab2707d229cb58add8835bb8c3dd6d6c56177
Emod Kovacs <emod.kovacs@doclerholding.com>
2019-11-19 16:56:59 +0100

    Updates

---
+++ precache-manifest.2d79a90a9b5432024f9c350baad22c73.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "239391774efd45ff20976d2f2509acb7",
+    "url": "/npm-find-name/index.html"
+  },
+  {
+    "revision": "3ffdef232a060526d90c",
+    "url": "/npm-find-name/static/css/2.09ae8418.chunk.css"
+  },
+  {
+    "revision": "3ffdef232a060526d90c",
+    "url": "/npm-find-name/static/js/2.59676785.chunk.js"
+  },
+  {
+    "revision": "2ae07a4f1ce7d9bcba9a",
+    "url": "/npm-find-name/static/js/main.6e58ecc2.chunk.js"
+  },
+  {
+    "revision": "70b63c591807f1a96a90",
+    "url": "/npm-find-name/static/js/runtime-main.23a52f6e.js"
+  },
+  {
+    "revision": "6f7aeee8bb8ee61edfd134eef3152e22",
+    "url": "/npm-find-name/static/media/Npm-logo.6f7aeee8.svg"
+  }
+]);
